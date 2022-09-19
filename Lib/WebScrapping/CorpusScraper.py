c1a084d4d638278e717484413d2cc46bc2deaa7b
Miles Danswan <miles.danswan@gmail.com>
2022-09-20 03:30:53 +1000

    Modified: CorpusScraper to include GoogleScholarSearch method

--- Lib/WebScrapping/CorpusScraper.py
+++ Lib/WebScrapping/CorpusScraper.py
@@ -1,11 +1,12 @@
 from Lib.WebScrapping.GoogleSearchSpider import *
+from Lib.WebScrapping.GoogleScholarSearchSpider import *
 from Lib.WebScrapping.WebsiteKeywordAnalysisSpider import *
 from Lib.WebScrapping.SpiderInstance import *
 from Lib.WebScrapping.Webpage import Webpage
 from Lib.TextAnalytics.Methods import *
 from Lib.Visualization.WordCloud import *
 
-def get_tag_corpus(tags, pages):
+def get_tag_corpus(tags, pages=1, methods=['Search', 'Scholar'], publications=[], from_year='', to_year=''):
 
     webpages: list[Webpage] = list()
 
@@ -26,7 +27,11 @@ def get_tag_corpus(tags, pages):
                 webpage.textual_content = webpage_content[3]
                 break
 
-    create_and_run_spider(GoogleSearch, google_search_results_listener, base_url="https://www.google.com.au/search?q={0}&start={1}", tags=tags, pages=pages)
+    if 'Search' in methods:
+        create_and_run_spider(GoogleSearch, google_search_results_listener, base_url="https://www.google.com.au/search?q={0}&start={1}", tags=tags, pages=pages)
+
+    if 'Scholar' in methods:
+        create_and_run_spider(GoogleScholarSearch, google_search_results_listener, tags=tags, pages=pages, pubs=[], from_year=from_year, to_year=to_year)
 
     for webpage in webpages:
         create_and_run_spider(WebsiteKeywordAnalysis, keyword_analysis_results_listener, base_url=webpage.url)
